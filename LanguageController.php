75689ac19a59c6e314797163ae549a464e6d4c41
gurzil300 <42491233+gurzil300@users.noreply.github.com>
2018-08-18 12:53:43 +0200

    Update LanguageController.php

--- LanguageController.php
+++ LanguageController.php
@@ -9,19 +9,20 @@ use App;
 class LanguageController extends Controller
 {
     public function change($lang) {
+        //languages you will be using in your app.
+        $languages = ['de','en','ba','fr','es'];
 
-        //App languages
-        $languages = ['de','en','ba','fr'];
-
+        //Chech if language in link exist in array of languages you will be using in your app
         if(in_array($lang, $languages)) {
-
             //Set cookie for furder check
+            //We use this to make sure language doesnt change on every App boot
+            //Cookie will expire in one week
             Cookie::queue(Cookie::make('lang', $lang , '10080'));
             //Redirect back
             return back();
 
         } else {
-            //For good measure
+            //We use this for good measure
             //Set default language
             App::setLocale(App::getLocale()); 
             //Redirect back
