e03b84f9f19b2feeb753df4c167060f14a78683d
gurzil300 <42491233+gurzil300@users.noreply.github.com>
2018-08-18 12:53:20 +0200

    Update AppServiceProvider.php

--- AppServiceProvider.php
+++ AppServiceProvider.php
@@ -15,18 +15,20 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
-    {    
-        //INFO
+     public function boot(){    
+        //Check for 'lang' cookie
         $cookie = Cookie::get('lang');
+        //Get visitors IP
         $ip = \Request::ip();
+        //Get visitors Geo info based on his IP
         $geo = GeoIP::getLocation($ip);
+        //Get visitors country name
         $country = $geo['country'];
 
-        //PREPARED COUNTRY LANGUAGES
-
+        //Prepared language based on country name
         $languages = [
             'United States' => 'en',
+            'Canada' => 'en',
             'Germany' => 'de',
             'Bosnia and Herzegovina' => 'ba',
             'Croatia' => 'ba',
@@ -35,24 +37,25 @@ class AppServiceProvider extends ServiceProvider
             'Luxembourg' => 'de',
         ];
 
-        if(isset($cookie) && !empty($cookie)) {
-            //Set language
+        if(!isset($cookie) && !empty($cookie)) {
+            //If cookie exist change application language
+            //We use this for good measure
             App::setLocale($cookie); 
         }else {
+            //If cookie doesnt exist
+            //Check country name in languages array
             if (array_key_exists($country, $languages)) {
-                //GET VALUE FROM ARRAY
+                //Get country value(language) from array
                 $lang = $languages[$country];
-                //Set language
+                //Set language based on value
                 App::setLocale($lang); 
 
             }
             else {
-                //Set language
+                //Set language for good measure
                 App::setLocale(App::getLocale()); 
             }
         }
-
-
     }
 
     /**
